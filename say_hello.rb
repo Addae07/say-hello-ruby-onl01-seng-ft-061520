91e19bebffddc683a0ea9d43858483a01ec8d191
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-17 17:03:48 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,3 +2,10 @@ def say_hello (name)
   puts "Hello #{name}!"
 end
 say_hello ("kent back")
+def say_hello(name)
+puts "Hello Kent Beck"
+end
+
+def say_hello(name="Ruby Programmer!")
+  puts ("Hello #{name}!")
+end
